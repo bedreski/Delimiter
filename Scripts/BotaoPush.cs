55ce8e1def3d2e3ddf9d8a4b75f89fa8d95c01b3
bedreski <fabianabedreski@gmail.com>
2021-06-30 13:42:16 -0300

    to push a box and advance with point automatically

---
+++ Scripts/BotaoPush.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//Revisado 
+
+public class BotaoPush : MonoBehaviour {
+
+    ControleJogo cj;  
+    Expressao e; 
+    Mensagem m; 
+    Ponto p; 
+    BotaoPush b; 
+
+    void Start() {
+
+        cj = GameObject.Find("ControleDoJogo").GetComponent<ControleJogo>();
+        e = GameObject.Find("expressao").GetComponent<Expressao>();
+        m = GameObject.Find("mensagem").GetComponent<Mensagem>();
+        p = GameObject.Find("pontoDeLocalizacao4dot5").GetComponent<Ponto>();
+        b = GameObject.Find("BotaoPush").GetComponent<BotaoPush>(); 
+    }
+
+    public void CaixaNaPilha() {
+
+        if(!Ponto.avançou) {
+
+            Debug.Log("Não é possível empilhar o mesmo caractere duas vezes!"); 
+
+        } else {
+
+
+            if(e.GetEncAbertura()) {
+
+                Caixa.SoltarCaixa(); 
+                Caixa.PousoDaCaixa(); 
+                cj.Empilha();
+                Ponto.avançou = false; 
+                StartCoroutine(TempoEspera()); 
+
+            } else {
+
+                m.StringParaText("Apenas delimitadores de abertura podem ser empilhados. Avance na expressão!");
+                StartCoroutine(m.WaitAndPrint(0.5f));
+
+            } 
+        }
+    }
+
+    public IEnumerator TempoEspera() {
+
+        yield return new WaitForSecondsRealtime(1);
+        p.MovePonto(); 
+    }
+}
+ 
